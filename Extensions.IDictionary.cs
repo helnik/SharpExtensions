2b66e7261d88a04b997d1051b1f76b08d84159be
Nick Chelmis <nikos.helmis@gmail.com>
2017-02-23 17:57:14 +0200

    Dictionary extension

---
+++ Extensions.IDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExtensions
+{
+    public partial class Extensions
+    {
+        /// <summary>
+        /// Retuns the value if key exist else add the value
+        /// </summary>      
+        public static TValue AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> dic, TKey key, TValue value)
+        {
+            if (!dic.ContainsKey(key))
+            {                
+                dic.Add(key, value);
+            }
+            else
+            {
+                dic[key] = value;
+            }
+
+            return dic[key];
+        }
+
+        /// <summary>
+        /// returns the value if key exists else adds it
+        /// </summary>      
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dic, TKey key, TValue value)
+        {
+            if (!dic.ContainsKey(key))
+            {
+                dic.Add(key, value);
+            }
+
+            return dic[key];
+        }
+    }
+}
