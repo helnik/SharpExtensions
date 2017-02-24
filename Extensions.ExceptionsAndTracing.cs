38a2d0abce8d967762be68b4bfe36a1a7ad8d593
Nick Chelmis <nikos.helmis@gmail.com>
2017-02-24 13:27:06 +0200

    Add files via upload

---
+++ Extensions.ExceptionsAndTracing.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExtensions
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// returns a list of all inner exceptions of the given exception
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static List<Exception> GetAllInners(this Exception ex)
+        {
+            List<Exception> exceptions = new List<Exception>();
+            Exception inner = ex;
+            while (inner != null)
+            {
+                exceptions.Add(inner.InnerException);
+                inner = inner.InnerException;
+            }
+            return exceptions;
+        }
+
+        /// <summary>
+        /// gets ths deepest inner exception
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static Exception GetMostInner(this Exception ex)
+        {
+            Exception inner = ex;
+
+            while (inner != null)
+            {
+                inner = inner.InnerException;
+                if (inner != null)
+                    ex = inner;
+            }
+            return ex;
+        }
+
+        /// <summary>
+        /// returns a string to log
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static string ToLoggableString(this Exception ex, string extraMessage = "")
+        {
+            StringBuilder log = new StringBuilder(DateTime.Now.ToString($"yyyy-MM-dd HH:mm:ss.fff : "));
+
+            if (ex.Source != null) log.Append($"at {ex.Source} ");
+
+            if (!string.IsNullOrEmpty(extraMessage)) log.Append($"{extraMessage} ");
+
+            log.Append("Exception Messages= ");
+            Exception newEx = ex;
+            int i = 1;
+            while (newEx != null)
+            {
+                log.Append($"#{i}: {ex.Message} ");
+                i++;
+                newEx = newEx.InnerException;
+            }
+
+            log.Append($"Exception Stacktrace = {ex.StackTrace} ");
+
+            Exception exBase = ex.GetBaseException();
+            if (exBase != null) log.Append($"Base Exception= {exBase}");
+
+            return log.ToString();
+        }
+
+        public static string ToJSON(this Exception ex)
+        {
+            ///requires Newtonsoft.Json nuget
+            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings();
+            settings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            var text = Newtonsoft.Json.JsonConvert.SerializeObject(ex, settings);
+            return text;
+        }
+
+        public static void ToEventLog(this Exception ex, string eventSource, EventLogEntryType logType = EventLogEntryType.Error)
+        {
+            try
+            {
+                EventLog.WriteEntry(
+                    eventSource,
+                    ex.Message,                   
+                    logType);
+            }
+            catch
+            {                
+                throw new Exception("Error Logging Exception", ex);
+            }
+        }
+    }
+}
