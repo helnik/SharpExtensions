86fe41d6e1ee5d4237aafd3a105f2710021538b1
Nick Chelmis <nikos.helmis@gmail.com>
2018-04-13 12:53:42 +0300

    Add IEnumerable ContentToString method

--- Extensions.IEnumerable.cs
+++ Extensions.IEnumerable.cs
@@ -9,5 +9,29 @@ namespace CustomExtensions
         {
             return collection == null || !collection.Any();
         }
+        
+        /// <summary>
+        /// Outputs the number of the elements and elements in a string 
+        /// </summary>        
+        /// <param name="collection">The IEnumerable collection </param>
+        /// <param name="separator">Separator of the elements. Default is comma followed by space </param>
+        /// <returns></returns>
+        private static string ContentToString<T>(this IEnumerable<T> collection, string separator = ", ")
+        {
+            if (collection.IsNullOrEmpty()) return $"Collection contains zero elements";
+            string output = string.Empty;
+            bool isFirst = true;
+
+            foreach (var element in collection)
+            {
+                if (isFirst)
+                {
+                    output += element.ToString();
+                    isFirst = false;
+                }
+                else output += separator + element.ToString();                
+            }
+            return $"Collection contains {collection.Count()} elements: {output} ";
+        }
     }
 }
